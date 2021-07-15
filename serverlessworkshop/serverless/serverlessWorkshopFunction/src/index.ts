ddd379274a8916c3c9a5ee51423759551136fe98
CHEN KAHANA <chenk@wix.com>
2021-07-15 11:09:17 +0300

    #serverless serverlessWorkshopFunction

--- serverlessworkshop/serverless/serverlessWorkshopFunction/src/index.ts
+++ serverlessworkshop/serverless/serverlessWorkshopFunction/src/index.ts
@@ -1,7 +1,15 @@
+import { MsmServer } from '@wix/ambassador-msm-server/rpc';
 import type { FunctionsBuilder } from '@wix/serverless-api';
 import { FullHttpResponse } from '@wix/serverless-api';
 
 module.exports = function builder (builder: FunctionsBuilder) {
   return builder
-    .addWebFunction('GET', '/hello', async () => new FullHttpResponse({ status: 200, body: 'hello, serverless' }));
+    .addWebFunction('GET', '/hello', async () => new FullHttpResponse({ status: 200, body: 'hello, serverless' }))
+    .addWebFunction('GET', '/isfree/:sitename', async (ctx, req) => {
+      const grpcClient = MsmServer().MetaSiteReadApi()(ctx.aspects);
+      const siteName = req.params.sitename;
+      return await grpcClient.isSiteNameFree({
+        siteName,
+      });
+    });
 };
--- serverlessworkshop/serverless/serverlessWorkshopFunction/test/index.spec.ts
+++ serverlessworkshop/serverless/serverlessWorkshopFunction/test/index.spec.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 describe('hello, serverless', () => {
   const testkit = app({});
   testkit.beforeAndAfter(10000);
-  
   it('should say hello', async () => {
     const result = await axios.get(testkit.getUrl('/hello'));
     expect(result.data).toStrictEqual('hello, serverless');
